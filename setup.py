5597e328b3a01ed644b8d65da02c14236f69f1b0
Valentine Svensson <v@nxn.se>
2015-05-25 00:34:57 +0100

    Added authors and URL

--- setup.py
+++ setup.py
@@ -9,4 +9,7 @@ setup(
         description='Python port of the R wrapper for the g:Profiler functional enrichment tool.',
         packages=find_packages(),
         install_requires=['requests', 'pandas'],
+        url='https://github.com/vals/python-gprofiler',
+        author='Valentine Svensson',
+        author_email='valentine@nxn.se'
     )
